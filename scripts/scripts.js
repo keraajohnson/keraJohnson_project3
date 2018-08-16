cac6cd15ed2fb21cc5c8c3c01fd905ddbc57533c
Kera <kera.andrea@gmail.com>
2018-08-15 21:19:13 -0400

    Major functionality changes and some style changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,277 +1,316 @@
 
-const messages = [
+const myApp = {}
+
+myApp.messages = [
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'reallyHot',
-        message: 'the copy for a friendship-over, just a friend, really-hot'
+        message: function (personName) {
+            return `Hey ${personName}, I know that we're supposed to hang out tonight but TBH it's really hot outside. Oh also, I just don't want to...So, bye 👋`}          
     },
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'betterOffer',
-        message: 'the copy for a friendshipOver just a friend and better offer'
+        message: function (personName) { 
+            return `Oh hi ${personName}, I "think" we were supposed to hang out tonight but I need to tell you that I have a better offer on the table and... I'm just going to take it. Sorry, not sorry!`}
     },
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver just a friend called back to mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver just a friend called back to mothership`}
     },
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'dogRoll',
-        message: 'the copy for a friendship over just a friend dog roll'
+        message: function (personName) { 
+            return `${personName} the copy for a friendship over just a friend dog roll`}
     },
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'napRecover',
-        message: 'the copy for a friendship over just a friend nap recover'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over just a friend nap recover`}
     },
     {
         tone: 'friendshipOver',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'actualExcuse',
-        message: 'the copy for a friendshipover just a friend actual excuse'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipover just a friend actual excuse`}
     },
     {
         tone: 'friendshipOver', 
         relation: 'family', 
         why: 'reallyHot',
-        message: 'the copy for a friendship over family member really hot'
+        message: function (personName) { 
+            return `${personName} the copy for a friendship over family member really hot`}
     },
     {
         tone: 'friendshipOver',
         relation: 'family',
         why: 'betterOffer', 
-        message: 'the copy for a friendship over family member better offer'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over family member better offer`}
     },
     {
         tone: 'friendshipOver',
         relation: 'family',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver family member who got called back to the mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who got called back to the mothership`}
     },
     {
         tone: 'friendshipOver',
         relation: 'family',
         why: 'dogRoll',
-        message: 'the copy for a friendshipOver family member whos dog rolled over'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member whos dog rolled over`}
     },
     {
         tone: 'friendshipOver',
         relation: 'family',
         why: 'napRecover',
-        message: 'the copy for a friendshipOver family member who needs to recover from a nap' 
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who needs to recover from a nap`} 
     },
     {
         tone: 'friendshipOver',
         relation: 'family',
         why: 'actualExcuse', 
-        message: 'the copy for a friendshipOver family member who has an actual excuse '
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who has an actual excuse `}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'reallyHot',
-        message: 'the copy for a friendship over partner member really hot'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over partner member really hot`}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'betterOffer',
-        message: 'the copy for a friendship over partner member better offer'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over partner member better offer`}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver partner member who got called back to the mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver partner member who got called back to the mothership`}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'dogRoll',
-        message: 'the copy for a friendshipOver partner member whos dog rolled over'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver partner member whos dog rolled over`}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'napRecover',
-        message: 'the copy for a friendshipOver partner member who needs to recover from a nap'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver partner member who needs to recover from a nap`}
     },
     {
         tone: 'friendshipOver',
         relation: 'partner',
         why: 'actualExcuse',
-        message: 'the copy for a friendshipOver family member who has an actual excuse '
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who has an actual excuse`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'reallyHot',
-        message: 'the copy for a friendship-over, just a friend, really-hot'
+        message: function (personName) {
+            return `${personName} the copy for a friendship-over, just a friend, really-hot`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'betterOffer',
-        message: 'the copy for a friendshipOver just a friend and better offer'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver just a friend and better offer`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver just a friend called back to mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver just a friend called back to mothership`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'dogRoll',
-        message: 'the copy for a friendship over just a friend dog roll'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over just a friend dog roll`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
         why: 'napRecover',
-        message: 'the copy for a friendship over just a friend nap recover'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over just a friend nap recover`}
     },
     {
         tone: 'friendsStill',
-        relation: 'justFriend',
+        relation: 'friend',
+        emoji: '🍟',
+        name: '"Friend"',
         why: 'actualExcuse',
-        message: 'the copy for a friendshipover just a friend actual excuse'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipover just a friend actual excuse`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'reallyHot',
-        message: 'the copy for a friendship over family member really hot'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over family member really hot`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'betterOffer',
-        message: 'the copy for a friendship over family member better offer'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over family member better offer`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver family member who got called back to the mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who got called back to the mothership`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'dogRoll',
-        message: 'the copy for a friendshipOver family member whos dog rolled over'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member whos dog rolled over`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'napRecover',
-        message: 'the copy for a friendshipOver family member who needs to recover from a nap'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who needs to recover from a nap`}
     },
     {
         tone: 'friendsStill',
         relation: 'family',
         why: 'actualExcuse',
-        message: 'the copy for a friendshipOver family member who has an actual excuse '
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver family member who has an actual excuse`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'reallyHot',
-        message: 'the copy for a friendship over partner member really hot'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over partner member really hot`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'betterOffer',
-        message: 'the copy for a friendship over partner member better offer'
+        message: function (personName) {
+            return `${personName} the copy for a friendship over partner member better offer`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'calledBack',
-        message: 'the copy for a friendshipOver partner member who got called back to the mothership'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver partner member who got called back to the mothership`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'dogRoll',
-        message: 'the copy for a friendshipOver partner member whos dog rolled over'
+        message: function (personName) {
+            return `${personName} the copy for a friendshipOver partner member whos dog rolled over`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'napRecover',
-        message: 'the copy for a friendshipOver partner member who needs to recover from a nap'
+        message: function (personName) {
+            return `${personName} hey the copy for a friendshipOver partner member who needs to recover from a nap`}
     },
     {
         tone: 'friendsStill',
         relation: 'partner',
         why: 'actualExcuse',
-        message: 'the copy for a friendshipOver family member who has an actual excuse '
+        message: function (personName) {
+            return `${personName} the  copy for a friendshipOver partner= who has an actual excuse`}
     }
 ]
 
+myApp.relationshipInfo = {
+    partner: 'BAE 💋',
+    friend: ' FRIEND 👯‍♀️',
+    family: ' FAM 👨‍👩‍👧‍👦 '
+}
 
-$(function() {
-    $('form').on('submit', function(e){
+// myApp.realtionshipImages = {
+//     partner: ''
+// }
+
+myApp.changeHeader = function () {
+    $('.relationship').on('click', function() { 
+        const relationShipSelection = $(this).val();
+        const objectSelection = myApp.relationshipInfo[relationShipSelection];
+        $('#emoji').html(objectSelection);
+    });
+};
+
+// use the variable of what the person has just clicked on to get it printed to the page 
+myApp.submitChoice = function () {
+    $('form').on('submit', function (e) {
         // preventing submit default 
         e.preventDefault();
         // variable to determine what the users realtionship is 
-        const personName= $('input[name=who-bail]').val();
-        const personPhoneNumber = $('input[name=phone-number]').val();
-        const userRelationChoice = $('input[name=who-are-they]:checked').val();
+        myApp.personName = $('input[name=who-bail]').val();
+        myApp.personPhoneNumber = $('input[name=phone-number]').val();
+        myApp.userRelationChoice = $('input[name=who-are-they]:checked').val();
         // variable to determine what the users reason why choice is 
-        const userWhyBailChoice = $('input[name=why-bail]:checked').val();
+        myApp.userWhyBailChoice = $('input[name=why-bail]:checked').val();
         // variable to determine what the users tone choice was 
-        const userToneChoice = $('input[name=tone]:checked').val();
-        console.log(personName, personPhoneNumber, userRelationChoice, userWhyBailChoice, userToneChoice);
+        myApp.userToneChoice = $('input[name=tone]:checked').val();
+        // console.log(personName, personPhoneNumber, userRelationChoice, userWhyBailChoice, userToneChoice);
 
         // filter method to determine what object should display
-        const firstFilteredArray = messages.filter(message => message.relation === userRelationChoice);
+        myApp.firstFilteredArray = myApp.messages.filter(message => message.relation === myApp.userRelationChoice);
         // console.log(firstFilteredArray);
-        const secondFilteredArray = firstFilteredArray.filter(message => message.why === userWhyBailChoice);
+        myApp.secondFilteredArray = myApp.firstFilteredArray.filter(message => message.why === myApp.userWhyBailChoice);
         // console.log(secondFilteredArray);
-        const finalFilteredArray = secondFilteredArray.filter(message => message.tone === userToneChoice);
-        console.log(finalFilteredArray);
+        myApp.finalFilteredArray = myApp.secondFilteredArray.filter(message => message.tone === myApp.userToneChoice);
+        console.log(myApp.finalFilteredArray);
 
-        $('.your-excuse').html(`<p>${finalFilteredArray}</p>`);
-    });
+        $('.your-excuse').html(`<p>${myApp.finalFilteredArray[0].message(myApp.personName)}</p>`);
+        });
+    };    
+
+
+
+myApp.init = function () {
+    myApp.changeHeader();
+    myApp.submitChoice();
+}
+
+$(function() {
+    myApp.init(); 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// I want to create a document.ready
-    // listen for the form submit
-        // I want to prevent the default action on the from submit 
-        // I want to create variables for the users choices:  
-            // name of the person
-            // phone number ??
-        // This is what we're going to use to get the right message from our array of messages 
-            // who they are to the user (userChoiceWho)
-            // why they're bailing (userChoiceWhy)
-            // what the tone of the message
-        // check each of the objects in the messages array 
-
-
-
-
     
